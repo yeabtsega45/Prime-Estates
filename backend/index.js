a1a5777994d1e611f56537549fb0a17ed8132a37
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-08 15:32:28 +0300

    connecting database

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,16 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-mongoose.connect(process.env.MONGO_URL, () =>
-  console.log("mongodb has been connecteded successfully")
-);
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database has been connected successfully");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+connectDb();
 
 app.listen(process.env.PORT, () =>
   console.log("server has been started successfully!")
