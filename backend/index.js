698c11c534951242b53072b72c9cc2f92e590d19
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-08 14:11:32 +0300

    connecting mongodb & listening to server

--- backend/index.js
+++ backend/index.js
@@ -4,3 +4,10 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
+mongoose.connect(process.env.MONGO_URL, () =>
+  console.log("mongodb has been connecteded successfully")
+);
+
+app.listen(process.env.PORT, () =>
+  console.log("server has been started successfully!")
+);
