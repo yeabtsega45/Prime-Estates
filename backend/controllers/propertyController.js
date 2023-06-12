b7cbcc4bd5ef4f84f474919dcb007cbfadc37017
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-12 16:11:37 +0300

    update property

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -110,7 +110,7 @@ propertyController.get("/find/:id", async (req, res) => {
   }
 });
 
-// create estate
+// create property
 propertyController.post("/", verifyToken, async (req, res) => {
   try {
     const newProperty = await Property.create({
@@ -119,6 +119,28 @@ propertyController.post("/", verifyToken, async (req, res) => {
     });
 
     return res.status(201).json(newProperty);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// update property
+propertyController.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const property = await Property.findById(req.params.id);
+    if (property.currentOwner.toString() !== req.user.id) {
+      throw new Error(
+        "You are not allowed to update other people's properties"
+      );
+    }
+
+    const updatedProperty = await Property.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedProperty);
   } catch (error) {
     return res.status(500).json(error);
   }
