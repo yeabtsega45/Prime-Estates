62eefe108fda56ce0f909333164784714ea84336
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-21 20:42:07 +0300

    fixing error on property details

--- client/src/components/propertyDetail/PropertyDetail.jsx
+++ client/src/components/propertyDetail/PropertyDetail.jsx
@@ -9,31 +9,27 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { request } from "../../util/fetchAPI";
 import { FaBed, FaSquareFull } from "react-icons/fa";
-import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
 import { useRef } from "react";
-import Comment from "../comment/Comment";
 
 const PropertyDetail = () => {
   const { token, user } = useSelector((state) => state.auth);
   const [propertyDetail, setPropertyDetail] = useState(null);
   const [showForm, setShowForm] = useState(false);
   const [desc, setDesc] = useState("");
-  const [isBookmarked, setIsBookmarked] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [commentText, setCommentText] = useState("");
-  const [comments, setComments] = useState([]);
   // todo display message
-  const [shortComment, setShortComment] = useState(false);
   const { id } = useParams();
   const formRef = useRef();
   const navigate = useNavigate();
+
+  const serviceId = process.env.REACT_APP_SERVICE_ID;
+  const templateId = process.env.REACT_APP_TEMPLATEE_ID;
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
   useEffect(
     (user) => {
       const fetchDetails = async () => {
         try {
           const data = await request(`/property/find/${id}`, "GET");
-          setIsBookmarked(data?.bookmarkedUsers?.includes(user?._id));
           setPropertyDetail(data);
         } catch (error) {
           console.error(error);
@@ -44,18 +40,6 @@ const PropertyDetail = () => {
     [id]
   );
 
-  useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const data = await request(`/comment/${id}`, "GET");
-        setComments(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchComments();
-  }, [id]);
-
   const handleCloseForm = () => {
     setShowForm(false);
     setDesc("");
@@ -64,25 +48,14 @@ const PropertyDetail = () => {
   const handleContactOwner = async (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_mjoebse",
-        "template_w5mthmm",
-        formRef.current,
-        "5T3Wb_hkHjKTOJDYQ"
-      )
-      .then(
-        (result) => {
-          handleCloseForm();
-          setSuccess(true);
-          setTimeout(() => {
-            setSuccess(false);
-          }, 2500);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    emailjs.sendForm(serviceId, templateId, formRef.current, publicKey).then(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
 
   const handleDelete = async () => {
@@ -91,46 +64,6 @@ const PropertyDetail = () => {
         Authorization: `Bearer ${token}`,
       });
       navigate("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleBookmark = async () => {
-    try {
-      await request(`/property/bookmark/${id}`, "PUT", {
-        Authorization: `Bearer ${token}`,
-      });
-      setIsBookmarked((prev) => !prev);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleComment = async () => {
-    if (commentText?.length < 2) {
-      setShortComment(true);
-      setTimeout(() => {
-        setShortComment(false);
-      }, 2500);
-      return;
-    }
-
-    try {
-      const options = {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      };
-
-      const newComment = await request(`/comment`, "POST", options, {
-        text: commentText,
-        listing: id,
-      });
-      setComments((prev) => {
-        return [newComment, ...prev];
-      });
-
-      setCommentText("");
     } catch (error) {
       console.log(error);
     }
@@ -207,31 +140,6 @@ const PropertyDetail = () => {
           <p className={classes.desc}>
             Desc: <span>{`${propertyDetail?.desc}`}</span>
           </p>
-          {user?._id != null &&
-            user?._id !== propertyDetail?.currentOwner?._id.toString() && (
-              <div className={classes.contactBookmarkControls}>
-                <button
-                  onClick={() => setShowForm(true)}
-                  className={classes.contactOwner}
-                >
-                  Contact owner
-                </button>
-                <span onClick={handleBookmark}>
-                  {isBookmarked ? (
-                    <BsFillBookmarkFill size={20} />
-                  ) : (
-                    <BsBookmark size={20} />
-                  )}
-                </span>
-              </div>
-            )}
-          {user?._id == null && (
-            <Link to={`/signin`}>
-              <h4 className={classes.noFuncMessage}>
-                Sign in to get access to the functionality.
-              </h4>
-            </Link>
-          )}
         </div>
       </div>
       {showForm && (
@@ -276,55 +184,6 @@ const PropertyDetail = () => {
           </div>
         </div>
       )}
-      {success && (
-        <div className={classes.success}>
-          You've successfully contacted the owner of the yacht!
-        </div>
-      )}
-      {shortComment && (
-        <div>
-          <div className={classes.error}>
-            Comment must be at least 2 characters long!
-          </div>
-        </div>
-      )}
-      <div className={classes.commentSection}>
-        {/* comment input */}
-        {user?._id == null && (
-          <h3 style={{ margin: "0.75rem", fontSize: "24px" }}>
-            Sign in to be able to comment!
-          </h3>
-        )}
-        {user?._id != null && (
-          <div className={classes.commentInput}>
-            <img
-              src={`http://localhost:5000/images/${user?.profileImg}`}
-              alt=""
-            />
-            <input
-              value={commentText}
-              type="text"
-              placeholder="Type message..."
-              onChange={(e) => setCommentText(e.target.value)}
-            />
-            <button onClick={handleComment}>Post</button>
-          </div>
-        )}
-        {/* displaying comments */}
-        <div className={classes.comments}>
-          {comments?.length > 0 ? (
-            comments?.map((comment) => (
-              <Comment
-                setComments={setComments}
-                key={comment._id}
-                comment={comment}
-              />
-            ))
-          ) : (
-            <h2 className={classes.noCommentsMessage}>No comments yet.</h2>
-          )}
-        </div>
-      </div>
     </div>
   );
 };
